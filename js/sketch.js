e7629a5cca2856915043f48df6fbe75d5f6f80e3
samira-sadat48 <samira.sadat48@gmail.com>
2021-09-01 15:23:14 -0600

    Clear grid button resets the grid background to white

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,9 @@
 const DEFAULT_SIZE = 16;
 
-const gridContainer = document.querySelector('.grid-container')
+const gridContainer = document.querySelector('.grid-container');
+const clearButton = document.querySelector('.clear-btn');
+
+clearButton.addEventListener('click',clearGrid, false);
 
 function hoverCell(e)
 {
@@ -19,6 +22,16 @@ function createGrid(sizeOfGrid)
         cell.addEventListener("mouseover", hoverCell, false);
         gridContainer.appendChild(cell);
     }
+}
+
+function clearGrid(e)
+{
+    const allCells = document.querySelectorAll(".cell");
+
+    allCells.forEach(function(cell) 
+    {
+        cell.style.backgroundColor = "white";
+    });
 }
 
 createGrid(DEFAULT_SIZE);
